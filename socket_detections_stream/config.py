30b5209812ffca78e0fc2940dffac15a08f7f1f2
Miew354 <michaelnotrack@duck.com>
2025-01-27 15:28:41 +1100

    updated default config

--- socket_detections_stream/config.py
+++ socket_detections_stream/config.py
@@ -1,17 +1,18 @@
-#put your model path here:
+#Model path:
 model = '/usr/share/imx500-models/imx500_network_efficientdet_lite0_pp.rpk'
 
-#detections labels path:
+#Detections labels path:
 labels_path = None #not required for default model
 
-#detection noise threshold
+#Detection noise threshold
 detection_threshold = 0.5
 
 #Socket streaming frequency in seconds
 stream_freq = 0.5
 
-#detection timeout in seconds
+#Detection timeout in seconds
+#Note: detection timeout only checked while a client is connected
 detection_timeout = 2
 
-#maximum items in the detection queue
-queue_maxsize = 100
+#Maximum items in the detection queue
+queue_maxsize = 10
