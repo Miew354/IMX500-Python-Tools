10768cf5128db27fac560e33f2b31c80fcdbedfa
Miew354 <michaelnotrack@duck.com>
2025-01-27 14:20:02 +1100

    fixed default labels file directory

--- install.sh
+++ install.sh
@@ -28,8 +28,8 @@ source IMX500-Python-Tools/bin/activate
 # Uninstall conflicting NumPy
 pip uninstall -y numpy
 
-# Download COCO labels for default detection model
-curl -o /usr/share/imx500-models/coco_labels.txt https://raw.githubusercontent.com/amikelive/coco-labels/refs/heads/master/coco-labels-paper.txt
+# Download COCO labels for default detections
+curl -o socket_detections_stream/coco_labels.txt https://raw.githubusercontent.com/amikelive/coco-labels/refs/heads/master/coco-labels-paper.txt
 
 echo "Setup complete. You can now run the application using './run.sh'."
 
--- socket_detections_stream/config.py
+++ socket_detections_stream/config.py
@@ -2,4 +2,4 @@
 model = '/usr/share/imx500-models/imx500_network_efficientdet_lite0_pp.rpk'
 
 #detections labels path:
-labels_path = '/usr/share/imx500-models/coco_labels.txt'
+labels_path = 'socket_detections_stream/coco_labels.txt'
